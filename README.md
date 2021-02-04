7677abfc31d0bb7b1c2ebc8006faf796bd8c68d9
Luis Cabrera Benito <parzibyte@gmail.com>
2021-02-03 22:53:14 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tomar_foto_flask_python
+ Tomar foto de la cámara con OpenCV, Python y Flask en una interfaz web. Dar opción de guardar en servidor o descargarla
---
+++ app.py
@@ -0,0 +1,100 @@
+"""
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me).
+------------------------------------------------------------------------------------------------
+Si el código es útil para ti, puedes agradecerme siguiéndome: https://parzibyte.me/blog/sigueme/
+Y compartiendo mi blog con tus amigos
+También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
+------------------------------------------------------------------------------------------------
+"""
+import uuid
+
+import cv2
+from flask import Flask, render_template, Response, jsonify
+
+app = Flask(__name__)
+
+# Si tienes varias cámaras puedes acceder a ellas en 1, 2, etcétera (en lugar de 0)
+camara = cv2.VideoCapture(0)
+
+
+# Una función generadora para stremear la cámara
+# https://flask.palletsprojects.com/en/1.1.x/patterns/streaming/
+def generador_frames():
+    while True:
+        ok, imagen = obtener_frame_camara()
+        if not ok:
+            break
+        else:
+            # Regresar la imagen en modo de respuesta HTTP
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + imagen + b"\r\n"
+
+
+def obtener_frame_camara():
+    ok, frame = camara.read()
+    if not ok:
+        return False, None
+    # Codificar la imagen como JPG
+    _, bufer = cv2.imencode(".jpg", frame)
+    imagen = bufer.tobytes()
+    return True, imagen
+
+
+# Cuando visiten la ruta
+@app.route("/streaming_camara")
+def streaming_camara():
+    return Response(generador_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# Cuando toman la foto
+@app.route("/tomar_foto_descargar")
+def descargar_foto():
+    ok, frame = obtener_frame_camara()
+    if not ok:
+        abort(500)
+        return
+    respuesta = Response(frame)
+    respuesta.headers["Content-Type"] = "image/jpeg"
+    respuesta.headers["Content-Transfer-Encoding"] = "Binary"
+    respuesta.headers["Content-Disposition"] = "attachment; filename=\"foto.jpg\""
+    return respuesta
+
+
+@app.route("/tomar_foto_guardar")
+def guardar_foto():
+    nombre_foto = str(uuid.uuid4()) + ".jpg"
+    ok, frame = camara.read()
+    if ok:
+        cv2.imwrite(nombre_foto, frame)
+    return jsonify({
+        "ok": ok,
+        "nombre_foto": nombre_foto,
+    })
+
+
+# Cuando visiten /, servimos el index.html
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
